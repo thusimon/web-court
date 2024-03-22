38cfef9d6de1f42f8fa0ab8ccee244e3f6e7f0ef
Lu Xu <thusimon@gmail.com>
2024-03-21 22:35:51 -0700

    Update background.ts

--- src/background.ts
+++ src/background.ts
@@ -201,9 +201,9 @@ function iou(box1: number[],box2: number[]) {
 function union(box1: number[],box2: number[]) {
   const [box1_x1,box1_y1,box1_x2,box1_y2] = box1;
   const [box2_x1,box2_y1,box2_x2,box2_y2] = box2;
-  const box1_area = (box1_x2-box1_x1)*(box1_y2-box1_y1)
-  const box2_area = (box2_x2-box2_x1)*(box2_y2-box2_y1)
-  return box1_area + box2_area - intersection(box1,box2)
+  const box1_area = (box1_x2-box1_x1)*(box1_y2-box1_y1);
+  const box2_area = (box2_x2-box2_x1)*(box2_y2-box2_y1);
+  return box1_area + box2_area - intersection(box1,box2);
 }
 
 function intersection(box1: number[],box2: number[]) {
@@ -213,7 +213,7 @@ function intersection(box1: number[],box2: number[]) {
   const y1 = Math.max(box1_y1,box2_y1);
   const x2 = Math.min(box1_x2,box2_x2);
   const y2 = Math.min(box1_y2,box2_y2);
-  return (x2-x1)*(y2-y1)
+  return (x2-x1)*(y2-y1);
 }
 
 const contextMenuClickHandler = async (info: Menus.OnClickData, tab: Tabs.Tab) => {
